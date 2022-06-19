d3cdb42620b8ad7f11e99bc14d2c7be99ebe1bdc
nhs-wobuzhidao <anoushka@ucsb.edu>
2022-06-18 21:51:04 -0700

    fb?

---
+++ fireBase.js
@@ -0,0 +1,29 @@
+var fireBase = fireBase || firebase;
+var hasInit = false;
+var config = {
+    apiKey: "AIzaSyAq5EuwGXS1vRpD8vdmvTQnRWHT3r8nIpA",
+    authDomain: "aimhelper-8510a.firebaseapp.com",
+    databaseURL: "https://aimhelper-8510a.firebaseio.com",
+    projectId: "aimhelper-8510a",
+    storageBucket: "aimhelper-8510a.appspot.com",
+    messagingSenderId: "43997639659"
+  };
+if(!hasInit){
+    firebase.initializeApp(config);
+    hasInit = true;
+}
+ 
+// var app_fireBase = {}; 
+// (function(){
+//     var config = {
+//         apiKey: "AIzaSyCLWALH3BXzOW8i7DyLicKmShnk_wiM6L0",
+//         authDomain: "sphax-3618a.firebaseapp.com",
+//         projectId: "sphax-3618a",
+//         storageBucket: "sphax-3618a.appspot.com",
+//         messagingSenderId: "722978428398",
+//         appId: "1:722978428398:web:d1bfcb5ba9ab11a047aaff"
+//     };
+//     firebase.initializeApp(config);
+//   // Initialize Firebase
+//     app_fireBase = firebase;
+// })()
---
+++ login.js
@@ -0,0 +1,44 @@
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: 'app.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+            //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: 'index.html'
+  };
+  
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
+// (function(){
+// // FirebaseUI config.
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// var uiConfig = {
+//     signInFlow: 'popup',
+//     signInSuccessUrl: 'app.html',
+//     signInOptions: [
+//       // Leave the lines as is for the providers you want to offer your users.
+//             //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//             //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+//             //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+//             //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//             firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//             //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+//     ],
+//     // Terms of service url.
+//     tosUrl: 'app.html'
+//   };
+  
+//   // Initialize the FirebaseUI Widget using Firebase.
+//   var ui = new firebaseui.auth.AuthUI(firebase.auth());
+//   // The start method will wait until the DOM is loaded.
+//   ui.start('#firebaseui-auth-container', uiConfig);
+// })()
