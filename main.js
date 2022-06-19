4e5cc67266c1e9a7f5acc9b938575e90ff12e296
sasharebello <sasharebello19@gmail.com>
2022-06-19 20:21:31 +0530

    pages

--- main.js
+++ main.js
@@ -23,8 +23,36 @@ var init = function(){
         }
       });
 }
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbarid");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function goToByScroll(id){
+  // Reove "link" from the ID
+id = id.replace("link", "");
+  // Scroll
+$('html,body').animate({
+    scrollTop: $("#"+id).offset().top},
+    'slow');
+}
+
+$("#sidebar > ul > li > a").click(function(e) { 
+  // Prevent a page reload when a link is pressed
+e.preventDefault(); 
+  // Call the scroll function
+goToByScroll($(this).attr("id"));           
+});
     
 init();
 
 mainApp.logout = logtout;
 })();
+
